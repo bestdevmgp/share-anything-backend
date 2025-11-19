f9a5d191933c5962e07a869cf8efceb07dc730af
bestdevmgp <me@mingyu.dev>
2025-11-19 11:06:30 +0900

    project setting

---
+++ target/debug/deps/share_anything-345629d977da8110.d
@@ -0,0 +1,5 @@
+/Users/mingyupark/Desktop/Dev/share-anything/target/debug/deps/share_anything-345629d977da8110.d: src/main.rs
+
+/Users/mingyupark/Desktop/Dev/share-anything/target/debug/deps/libshare_anything-345629d977da8110.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/share_anything-959b8404454ac0b1.d
@@ -0,0 +1,5 @@
+/Users/mingyupark/Desktop/Dev/share-anything/target/debug/deps/share_anything-959b8404454ac0b1.d: src/main.rs
+
+/Users/mingyupark/Desktop/Dev/share-anything/target/debug/deps/libshare_anything-959b8404454ac0b1.rmeta: src/main.rs
+
+src/main.rs:
