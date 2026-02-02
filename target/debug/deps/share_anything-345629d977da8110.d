7ebc5b3809da19e8f8fcc32e2c7ad0e24c1ae48b
bestdevmgp <me@mingyu.dev>
2026-02-02 10:31:55 +0900

    chore: logging

--- target/debug/deps/share_anything-345629d977da8110.d
+++
@@ -1,5 +0,0 @@
-/Users/mingyupark/Desktop/Dev/share-anything/target/debug/deps/share_anything-345629d977da8110.d: src/main.rs
-
-/Users/mingyupark/Desktop/Dev/share-anything/target/debug/deps/libshare_anything-345629d977da8110.rmeta: src/main.rs
-
-src/main.rs:
--- target/debug/deps/share_anything-959b8404454ac0b1.d
+++
@@ -1,5 +0,0 @@
-/Users/mingyupark/Desktop/Dev/share-anything/target/debug/deps/share_anything-959b8404454ac0b1.d: src/main.rs
-
-/Users/mingyupark/Desktop/Dev/share-anything/target/debug/deps/libshare_anything-959b8404454ac0b1.rmeta: src/main.rs
-
-src/main.rs:
